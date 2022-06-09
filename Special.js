b746366e474f9b4d329a70cc88096a1e31094c95
SerhiiSa07 <875dfsb6435278785@gmail.com>
2022-06-09 18:46:18 +0300

    Is special number

--- Special.js
+++ Special.js
@@ -1,10 +1,10 @@
 function isSpecialNumber(n) {
-    let c = String(n)
-    const s = '012345';
-      for (const i of c) {
-        if(!s.includes(i)){
-          return 'NOT!!'
-        }
+  let c = String(n)
+  const s = '012345';
+    for (const i of c) {
+      if(!s.includes(i)){
+        return 'NOT!!'
       }
-      return 'Special!!'
-  }
+    }
+    return 'Special!!'
+}
